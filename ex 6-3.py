63ef5d91f7724b632b54fffbaddb00b2ca977626
sheldonruru <sheldonkao0620@gmail.com>
2025-04-22 19:46:10 +0800

    Create ex 6-3.py

---
+++ ex 6-3.py
@@ -0,0 +1,8 @@
+day = int (input("please num 1-7:"))
+match day:
+    case 1 | 2 | 3 | 4 | 5:
+        print("Today is a weekday")
+    case 6 | 7:
+        print("I love weekends!")
+    case _:
+        print("please input num 1-7! ")
